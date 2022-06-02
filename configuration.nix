1bae4b805e8efd33fb407fdcd3f66f1e7a2d037b
HoseongLee <leehs.git@gmail.com>
2022-06-02 16:04:49 +0900

    Moved audio configs to a seperate file and added Easyeffects

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,9 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports =
   [
+    ./audio.nix
     ./neovim.nix
     /etc/nixos/hardware-configuration.nix
   ];
@@ -107,24 +108,6 @@
 
     windowManager.awesome.enable = true;
 
-  };
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-        bluez_monitor.properties = {
-            ["bluez5.enable-sbc-xq"] = true,
-            ["bluez5.enable-msbc"] = true,
-            ["bluez5.enable-hw-volume"] = true,
-            ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-        }
-    '';
   };
 
   environment.sessionVariables = rec {
